9903fb08289e658b1c05ee6a15c4ff219a72bbfd
Luís Henrique Vanni <luishvanni@gmail.com>
2024-05-13 02:25:02 -0300

    Update Main.java

--- trabalho-final-poo/src/Main.java
+++ trabalho-final-poo/src/Main.java
@@ -2,6 +2,22 @@ public class Main {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
         sistema.incluirFornecedor();
+        sistema.excluirFornecedor();
+       
+        sistema.incluirProduto();
+        sistema.excluirProduto();
+
+        
+        sistema.consultarProdutoPorNome();
+        sistema.consultarProdutoPorCodigo();
+
+        sistema.consultarFornecedorPorNome();
+
+        sistema.alterarFornecedor();
+        sistema.alterarProduto();
+        
+
+
 
     }
 }
